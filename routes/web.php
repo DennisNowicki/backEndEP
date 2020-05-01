1857d55679d8a036b93ff827004963d153a489aa
dennis nowicki <dennis.nowicki@outlook.com>
2020-05-01 17:49:46 +0200

    Paperkit V2 layout

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
+});
+
+Route::get('/home', function () {
+    return view('layouts.home');
 });
